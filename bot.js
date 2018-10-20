405218e48fe0c577d8e02285dde934f6c839e743
Discord.js Arabic Developer <32039975+ShadwX57@users.noreply.github.com>
2018-10-20 13:06:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = "-"
-const code = '-'
+const prefix = "#"
+const code = '#'
 
 client.on('ready', () => {
 	client.user.setGame(`Flame Codes.`,'https://www.twitch.tv/fofodiscord');
